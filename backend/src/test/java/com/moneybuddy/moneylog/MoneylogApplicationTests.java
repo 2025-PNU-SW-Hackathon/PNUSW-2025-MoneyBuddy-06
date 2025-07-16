6b8fa33b14638023a39ff13b9073dc4295f6b2e8
구민서 <minseo12379@gmail.com>
2025-07-16 20:21:44 +0900

    chore: 프론트, 백 초기 설정

---
+++ backend/src/test/java/com/moneybuddy/moneylog/MoneylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneybuddy.moneylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
