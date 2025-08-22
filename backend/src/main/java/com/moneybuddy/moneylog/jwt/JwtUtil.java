3f098481d8693b3ac7966f40cd9e1e1d24471dea
leejihyeon83 <jihyun04@gmail.com>
2025-08-22 17:03:23 +0900

    feat: 로그아웃 기능 구현

---
+++ backend/src/main/java/com/moneybuddy/moneylog/jwt/JwtUtil.java
@@ -0,0 +1,60 @@
+package com.moneybuddy.moneylog.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key secretKey;
+    private final long expiration = 3600000L;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Claims parseToken(String token) throws JwtException {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getEmail(String token) {
+        Claims claims = parseToken(token);
+        return claims.get("email", String.class);
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = parseToken(token);
+        return claims.get("userId", Long.class);
+    }
+
+    public void validateToken(String token) throws JwtException {
+        parseToken(token);
+    }
+
+    public String createToken(Long userId,String email) {
+        long now = System.currentTimeMillis();
+        return Jwts.builder()
+                .claim("email", email)
+                .claim("userId", userId)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + expiration))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 토큰 발급 시간: 비밀번호 변경 시 토큰 비교
+    public Date getIssuedAt(String token) {
+        Claims claims = parseToken(token);
+        return claims.getIssuedAt();
+    }
+}
