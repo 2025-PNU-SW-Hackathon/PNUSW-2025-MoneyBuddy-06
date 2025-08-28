0ed0812d657749644fe8c2dc7b6a6d2dbe06902d
kimminseo <kmmlns@pusan.ac.kr>
2025-08-28 16:28:13 +0900

    Fix/challenge (#29)
    * fix: success 코드 수정
    
    * fix: API 실행 확인
    
    * fix: boolean null 문제 해결
    
    * fix: 버그 수정
    
    * fix: 버그 수정
    
    * fix: 버그수정
    
    * fix: 버그 수정
    
    * refactor: 추천 챌린지 제목 변경
    
    * feat: 챌린지 오늘 성공/최종 성공 변수 추가

--- backend/src/main/java/com/moneybuddy/moneylog/dto/response/ChallengeStatusResponse.java
+++ backend/src/main/java/com/moneybuddy/moneylog/dto/response/ChallengeStatusResponse.java
@@ -8,7 +8,8 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ChallengeStatusResponse {
-    private boolean success;   // API 처리 성공 여부
-    private String message;    // 안내 메시지
-    private int currentDay;    // 현재까지 몇 일 성공했는지
+    private String message;        // 안내 메시지
+    private int currentDay;        // 현재까지 몇 일 성공했는지
+    private boolean todaySuccess;  // 오늘 성공 여부
+    private boolean finalSuccess;  // 최종 챌린지 성공 여부
 }
