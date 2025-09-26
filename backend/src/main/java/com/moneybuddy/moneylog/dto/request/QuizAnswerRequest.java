2fee678a3b597bc41af599fbb8e6cd8daa477062
kimminseo <kmmlns@pusan.ac.kr>
2025-09-26 11:27:43 +0900

    feat: 사용자별 하루 1문제 배정 및 답변 제출 로직 구현 (#32)

--- backend/src/main/java/com/moneybuddy/moneylog/dto/request/QuizAnswerRequest.java
+++ backend/src/main/java/com/moneybuddy/moneylog/dto/request/QuizAnswerRequest.java
@@ -1,14 +1,10 @@
 package com.moneybuddy.moneylog.dto.request;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Getter @Setter
 public class QuizAnswerRequest {
-
-    private Long quizId;
-    private Boolean answer;
+    private Long quizId;     // 배정된 퀴즈 id
+    private Boolean answer;  // 사용자가 고른 답 (true/false)
 }
